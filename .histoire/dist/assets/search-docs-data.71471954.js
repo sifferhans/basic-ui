cdf3411992995279ef5824f282453b9f685d2d8a
Sigve Hansen <sighan12@gmail.com>
2022-07-13 22:46:27 +0200

    fix: remove dist

--- .histoire/dist/assets/search-docs-data.71471954.js
+++
@@ -1,6 +0,0 @@
-import { Z as markRaw } from "./vendor.50b6fd3b.js";
-let searchData$1 = { "index": { "reg": '{"0":1,"1":1}', "text.cfg": '{"doc":0,"opt":1}', "text.map": '[{"p":[0],"po":[0],"pot":[0],"poto":[0],"potom":[0],"e":[1],"em":[1],"emp":[1],"empo":[1],"empot":[1]},{"o":[0,1],"os":[0,1],"ose":[0,1],"oset":[0,1]},{"f":[0,1],"fo":[0,1],"for":[0,1]},{"oser":[0,1]},{"e":[0],"em":[0],"emt":[0],"emte":[0],"emter":[0],"emtera":[0],"emterak":[0],"emterakt":[0],"emterakte":[0],"emterakteo":[0],"emterakteom":[0],"emterakteoms":[0]},{"a":[1],"am":[1],"amt":[1]},{"form":[1],"forms":[1]},{},{}]', "text.ctx": "[{}]" }, "idMap": { "0": { "id": "src-components-button-button-story-vue", "kind": "story" }, "1": { "id": "src-components-input-input-story-vue", "kind": "story" } } };
-const searchData = markRaw(searchData$1);
-function onUpdate(cb) {
-}
-export { onUpdate, searchData };
