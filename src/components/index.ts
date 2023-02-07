a169d7a08abfd7d05c56e49045d51e1d51d0704b
Sigve Hansen <sighan12@gmail.com>
2023-02-07 23:13:59 +0000

    chore(components): move out of folders

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,13 @@
-import Button from './Button/Button.vue'
-import Checkbox from './Checkbox/Checkbox.vue'
-import Flex from './Flex/Flex.vue'
-import Form from './Form/Form.vue'
-import Grid from './Grid/Grid.vue'
-import Heading from './Heading/Heading.vue'
-import Input from './Input/Input.vue'
-import Select from './Select/Select.vue'
-import Switch from './Switch/Switch.vue'
-import Textarea from './Textarea/Textarea.vue'
+import Button from './Button.vue'
+import Checkbox from './Checkbox.vue'
+import Flex from './Flex.vue'
+import Form from './Form.vue'
+import Grid from './Grid.vue'
+import Heading from './Heading.vue'
+import Input from './Input.vue'
+import Select from './Select.vue'
+import Switch from './Switch.vue'
+import Textarea from './Textarea.vue'
 
 export {
 	Button,
